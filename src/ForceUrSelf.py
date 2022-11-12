f12d9a8a23d9c700d1732d10374eecd3bc27893f
Sami <lolipop66611@gmail.com>
2022-11-12 14:16:19 +0100

    Added basic bruteforcer

--- src/ForceUrSelf.py
+++ src/ForceUrSelf.py
@@ -12,9 +12,35 @@ __version__ = "1.0.0"
 OS = "windows" if "windows" in platform().lower() else "linux"
 
 # Set the username of the currently logged in user
-USER = os.getlogin()
+USER_OS = os.getlogin()
 
-def main(args: argparse.Namespace) -> None:
+# Bruteforce mode
+def bruteforce(username: str, password_list: str, URL, proxy: str) -> None:
+    print('Starting bruteforce mode...')
+    response = None
+    with open(password_list, 'r') as passwords:
+        for password in passwords:
+            password = password.strip()
+            if proxy:
+                proxies = {
+                    'http': proxy,
+                    'https': proxy
+                }
+                try:
+                    response = requests.get(URL, auth=(username, password), proxies=proxies)
+                except:
+                    print(colored(f'Error: Invalid proxy', 'red'))
+                    exit()
+            else:
+                response = requests.get(URL, auth=(username, password))
+            if response.status_code == 200:
+                print(colored(f'HIT: Password found: {password}', 'green'))
+                break
+            else:
+                continue
+    print('Finished bruteforce mode...')
+
+def main(args: list[str]) -> None:
     '''
     Tool: ForceUrSelf
     Author: Sami Hindi
@@ -23,7 +49,34 @@ def main(args: argparse.Namespace) -> None:
     License: GNU General Public Licesne V3.0
     OS: Windows & Linux
     '''
+    USERNAME: str = ''
+    PASSWORD_LIST: str = ''
+    PROXY: str = ''
+    MODE: str = ''
+    VERBOSE: bool = False
+    URL: str = ''
     
+    for arg in args:
+        if 'verbose' in arg:
+            VERBOSE = True
+        
+        if 'mode' in arg:
+            MODE = arg.split(':::')[1]
+            
+        if 'username' in arg:
+            USERNAME = arg.split(':::')[1]
+        
+        if 'password_list' in arg:
+            PASSWORD_LIST = arg.split(':::')[1]
+            
+        if 'proxy' in arg:
+            PROXY = arg.split(':::')[1]
+        
+        if 'url' in arg:
+            URL = arg.split(':::')[1]
+    
+    if MODE == 'bruteforce':
+        bruteforce(USERNAME, PASSWORD_LIST, URL, PROXY)
     
     
 if __name__ == '__main__':
@@ -50,10 +103,19 @@ if __name__ == '__main__':
     # Add verbosity argument
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Use verbose mode')
+
+    # Add URL argument
+    parser.add_argument('-url', '--url', type=str,
+                        help='URL to use for bruteforcing')
     
     # Parse the arguments
     args = parser.parse_args()
+    
+    # Handle arguments in parse args method
     parsed_args = methods.parse_args(args)
     
-    # Handle arguments in main function
+    # Run main method
     main(parsed_args)
+    
+    print(colored('ForceUrSelf has finished running.', 'green'))
+    
--- src/methods.py
+++ src/methods.py
@@ -10,15 +10,27 @@ def print_logo() -> None:
 
 def parse_args(args) -> list[str]:
     # Method to parse arguments
+    RETURN_LIST: list[str] = []
     print('Parameters: ')
     if args.verbose:
         print(colored(f'Verbose mode enabled', 'green'))
+        RETURN_LIST.append('verbose')
         
-    if args.mode == 'bruteforce':
+    if args.mode:
         print(colored(f'Running ForceUrSelf in bruteforce mode', 'green'))
+        RETURN_LIST.append(f'mode:::{args.mode}')
         
         if args.username and args.password_list:
-            print(colored(f'Username: {args.username}\nPassword list: {args.pl}', 'green'))
+            print(colored(f'Username: {args.username}\nPassword list: {args.password_list}', 'green'))
+            RETURN_LIST.append(f'username:::{args.username}')
+            RETURN_LIST.append(f'password_list:::{args.password_list}')
     
     if args.proxy:
         print(colored(f'Using proxy: {args.proxy}', 'green'))
+        RETURN_LIST.append(f'proxy:::{args.proxy}')
+        
+    if args.url:
+        print(colored(f'Using URL: {args.url}', 'green'))
+        RETURN_LIST.append(f'url:::{args.url}')
+
+    return RETURN_LIST
